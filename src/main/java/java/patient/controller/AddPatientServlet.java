2a2804276566d22bcba5d995b8672f2d861bb43a
soumyamh <elmachbouhsoumya854@gmail.com>
2026-02-24 16:39:42 +0100

    Add files via upload

--- src/main/java/java/patient/controller/AddPatientServlet.java
+++ src/main/java/java/patient/controller/AddPatientServlet.java
@@ -3,6 +3,7 @@ package java.patient.controller;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,19 +22,28 @@ public class AddPatientServlet extends HttpServlet {
 
         try (Connection con = DBContext.getConnection()) {
 
-            String sql = "INSERT INTO patient (id_patient, cin, nom, prenom, genre, phone, email, adress, medecin_traitant, date_creation, date_modification) "
-                       + "VALUES (seq_patient.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, SYSDATE, SYSDATE)";
+            String sql = "INSERT INTO patient "
+                    + "(id_patient, cin, nom, prenom, date_nais, genre, phone, email, adress, "
+                    + "medecin_traitant, date_creation, date_modification, cree_par, modifie_par) "
+                    + "VALUES (seq_patient.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, SYSDATE, SYSDATE, ?, ?)";
 
             PreparedStatement ps = con.prepareStatement(sql);
 
-            ps.setString(1, request.getParameter("cin"));      
+            ps.setString(1, request.getParameter("cin"));
             ps.setString(2, request.getParameter("nom"));
             ps.setString(3, request.getParameter("prenom"));
-            ps.setString(4, request.getParameter("genre"));
-            ps.setString(5, request.getParameter("phone"));
-            ps.setString(6, request.getParameter("email"));
-            ps.setString(7, request.getParameter("adress"));
-            ps.setString(8, request.getParameter("medecin"));
+
+           
+            ps.setDate(4, Date.valueOf(request.getParameter("date_nais")));
+
+            ps.setString(5, request.getParameter("genre"));
+            ps.setString(6, request.getParameter("phone"));
+            ps.setString(7, request.getParameter("email"));
+            ps.setString(8, request.getParameter("adress"));
+            ps.setString(9, request.getParameter("medecin"));
+
+            ps.setString(10, request.getParameter("cree_par"));
+            ps.setString(11, request.getParameter("modifie_par"));
 
             ps.executeUpdate();
 
--- src/main/java/java/patient/controller/SearchPatientServlet.java
+++ src/main/java/java/patient/controller/SearchPatientServlet.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,18 +15,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import patient.java.DBContext;
 
-/**
- * Servlet implementation class SearchPatientServlet
- */
 @WebServlet("/SearchPatientServlet")
 public class SearchPatientServlet extends HttpServlet {
 
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String cin = request.getParameter("cin");
@@ -33,8 +28,11 @@ public class SearchPatientServlet extends HttpServlet {
 
         try (Connection conn = DBContext.getConnection()) {
 
-            String sql = "SELECT id_patient, nom, prenom, phone, email, adress, medecin_traitant FROM patient WHERE cin = ?";
-            
+            String sql = "SELECT id_patient, nom, prenom, date_nais, genre, "
+                       + "phone, email, adress, medecin_traitant, "
+                       + "date_creation, date_modification, cree_par, modifie_par "
+                       + "FROM patient WHERE cin = ?";
+
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, cin);
 
@@ -46,10 +44,20 @@ public class SearchPatientServlet extends HttpServlet {
                 patient.setId(rs.getInt("id_patient"));
                 patient.setNom(rs.getString("nom"));
                 patient.setPrenom(rs.getString("prenom"));
+
+                patient.setDate_nais (rs.getDate("date_nais"));
+                patient.setGenre(rs.getString("genre"));
+
                 patient.setPhone(rs.getString("phone"));
                 patient.setEmail(rs.getString("email"));
                 patient.setAdress(rs.getString("adress"));
                 patient.setMedecin(rs.getString("medecin_traitant"));
+
+                patient.setDate_creation(rs.getDate("date_creation"));
+                patient.setDate_modification(rs.getDate("date_modification"));
+
+                patient.setCree_par(rs.getString("cree_par"));
+                patient.setModifie_par(rs.getString("modifie_par"));
             }
 
             request.setAttribute("patient", patient);
